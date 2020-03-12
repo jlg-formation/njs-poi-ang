2ed83d84c4959d3689052dc597146daa6c677948
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-12 17:41:43 +0100

    select grid

--- src/app/routes/list/list.component.ts
+++ src/app/routes/list/list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private elt: ElementRef) {
   }
 
+  ngOnInit(): void {}
+
+  delete() {
+    console.log('delete');
+  }
+
+  select(index: number) {
+    console.log('select', index);
+    const row: Element = this.elt.nativeElement.querySelector('.row-' + index);
+    if (!row.classList.contains('selected')) {
+      row.classList.add('selected');
+    } else {
+      row.classList.remove('selected');
+    }
+  }
 }
