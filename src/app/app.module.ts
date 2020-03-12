72e7d2b92063095ddef55d2798df940029a78119
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-12 16:39:29 +0100

    create 3 routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './routes/list/list.component';
+import { CreateComponent } from './routes/create/create.component';
+import { UpdateComponent } from './routes/update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
