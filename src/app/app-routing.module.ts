188aca8a5e798dfe51574e1a10605f90185eb1e5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-12 16:57:15 +0100

    loop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './routes/list/list.component';
+import { CreateComponent } from './routes/create/create.component';
+import { UpdateComponent } from './routes/update/update.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'update', component: UpdateComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
