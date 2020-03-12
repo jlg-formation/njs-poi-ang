66755aabda3637e6dfd35774aaafc01e28b47fef
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-12 17:08:04 +0100

    list ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,15 @@ import { ListComponent } from './routes/list/list.component';
 import { CreateComponent } from './routes/create/create.component';
 import { UpdateComponent } from './routes/update/update.component';
 
-
 const routes: Routes = [
   { path: '', component: ListComponent },
   { path: 'create', component: CreateComponent },
   { path: 'update', component: UpdateComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
